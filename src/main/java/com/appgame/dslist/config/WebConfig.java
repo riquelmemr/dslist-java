77b82fd017ecca057ffe3bb4a1ff9783a9b55a62
Riquelme Maia Rodrigues <riquelmemaia2@gmail.com>
2024-11-17 22:00:12 -0300

    Feat: Add WebConfig

---
+++ src/main/java/com/appgame/dslist/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.appgame.dslist.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+    @Value("${cors.origins}")
+    private String corsOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+            }
+        };
+    }
+
+}
